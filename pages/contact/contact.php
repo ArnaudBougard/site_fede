3b552eaab24a1247ddac1e8b8f827b6e2eea2b66
furtivesquirrel <terry.weber96@outlook.be>
2019-09-12 14:37:12 +0200

    AJout de css pour le formulaire de contact

--- pages/contact/contact.php
+++ pages/contact/contact.php
@@ -11,7 +11,7 @@ session_start();
 		<?php include("../head.php"); ?>
 	</head>
 
-	<body>
+	<body class="bg1">
 		
 		<?php include("../network.php"); ?>
 
@@ -23,51 +23,46 @@ session_start();
 
 
 
-<!-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
-
-
-
-
 <section class="center-block mb-0 bg-fourth" id="contact">
-    <div class="container-fluid bg-black">
-    <!--Section heading-->
-    <h2 class="h1-responsive font-weight-bold text-center my-4 txt-white">Formulaire de contact</h2>
-    <!--Section description-->
-    </div>
-
-    <div class="container mt">
-    
-   
+    <div class="container" style="margin-top: 2vw; margin-bottom: 4vw;">   
       <div class="row">
-        <div class="col-lg-12 mx-auto">
+        <div class=col-lg-5>
+          <img src="../../assets/img/contact.png" alt="contact" style="width: 55%; margin-top: 5vw;">
+        </div>
+        <div class="col-lg-7 mx-auto">
           <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
           <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
-          <form name="sentMessage" id="contactForm" novalidate="novalidate">
+          <div class="container-fluid">
+          <!--Section heading-->
+            <h2 class="contact-title">Contact</h2>
+          <!--Section description-->
+          </div>
+                <form name="sentMessage" id="contactForm" novalidate="novalidate">
             <div class="control-group">
               <div class="form-group floating-label-form-group controls mb-0 pb-2">
                 <label>Nom</label>
-                <input class="form-control "  id="name" type="text" placeholder="Nom & Prénom" required="required" data-validation-required-message="Veuillez completer le champ.">
+                <input class="form-control "  id="name" type="text" placeholder="" required="required" data-validation-required-message="Veuillez completer le champ.">
                 <p class="help-block text-danger"></p>
               </div>
             </div>           
             <div class="control-group">
               <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                <label>Adresse mail</label>
-                <input class="form-control" id="email" type="email" placeholder="Email" required="required" data-validation-required-message="Veuillez completer le champ.">
+                <label>E-mail</label>
+                <input class="form-control" id="email" type="email" placeholder="" required="required" data-validation-required-message="Veuillez completer le champ.">
                 <p class="help-block text-danger"></p>
               </div>
             </div>
             <div class="control-group">
               <div class="form-group floating-label-form-group controls mb-0 pb-2">
                 <label>Message</label>
-                <textarea class="form-control" id="message" rows="5" placeholder="Message" required="required" data-validation-required-message="Veuillez completer le champ."></textarea>
+                <textarea class="form-control" id="message" rows="5" placeholder="" required="required" data-validation-required-message="Veuillez completer le champ."></textarea>
                 <p class="help-block text-danger"></p>
               </div>
             </div>
             <br>
             <div id="success"></div>
             <div class="form-group">
-              <button type="submit" class="btn btn-primary btn-xl btn-outline-dark" id="sendMessageButton">Envoyer</button>
+              <button type="submit" class="btn-contact" id="sendMessageButton">Envoyer</button>
             </div>
           </form>
         </div>
